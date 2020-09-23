30d990fbcd5042701f093da4977540d116a877c0
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-09-23 21:47:51 +0200

    edited pyinstall

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
-cd ~
 export WORKON_HOME=~/virtualenvs
+
 mkdir -p $WORKON_HOME
+
 source /usr/bin/virtualenvwrapper.sh
+
 mkvirtualenv py3
-pip install ipython scipy numpy pandas matplotlib scikit-rf scikit-learn sympy chaospy
+
+install_pip() {
+    pip install $1
+}
+
+PIP_LIST=(
+    ipython
+    scipy
+    numpy
+    pandas
+    matplotlib
+    scikit-rf
+    scikit-learn
+    sympy
+    chaospy
+)
+
+for name in "${PIP_LIST[@]}"; do
+    install_pip $name
+done
+
 deactivate
